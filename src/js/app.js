ac80e1f39d8af66e239fe6dee59e6b41163d9ff4
Ceasar Lopez <155122341+CeasarLopez@users.noreply.github.com>
2024-03-02 00:52:56 +0000

    contional

--- src/js/app.js
+++ src/js/app.js
@@ -29,14 +29,20 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  const name = variables.name || "Your First Name";
+  const lastName = variables.lastName || "Your Last Name";
+  const role = variables.role || "Taco Man";
+  const city = variables.city || "San Diego";
+  const includeCover = variables.includeCover || "";
+  const socialMediaPosition = variables.socialMediaPosition || "position-right";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${name + " " + lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}</h3>
+          <ul class="${socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
